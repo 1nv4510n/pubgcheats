dbe4f66bcdc0ae2f331da9410920b4b0e310c67a
1nv4510n <bogdan22647axe@gmail.com>
2020-08-14 20:39:22 +0500

    first commit

---
+++ js/gtag.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-175453159-1');
+gtag('set', {'user_id': 'USER_ID'});
